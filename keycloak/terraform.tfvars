ec3f29e74e3b5195cc5051bbc3d82c06bd61660d
sahil.kamble@ayanworks.com <sahil.kamble@ayanworks.com>
2025-01-09 12:50:53 +0530

    Added tfvars file
    Signed-off-by: sahil.kamble@ayanworks.com <sahil.kamble@ayanworks.com>

---
+++ keycloak/terraform.tfvars
@@ -0,0 +1,8 @@
+realm = "credebl_realm" # Add Realm name
+access_token_lifespan = "48h"
+sso_session_idle_timeout = "48h"
+realm_role = "holder"
+root_url = "https://abc.com"  # Add Keycloak Endpoint
+redirect_url = "https://abc.com" # Add Redirect URL
+username = "admin"
+password = "admin"
--- keycloak/variables.tf
+++ keycloak/variables.tf
@@ -1,39 +1,31 @@
 variable "realm" {
   type = string
-  default = "credebl_realm"
 }
 
 variable "access_token_lifespan" {
   type = string
-  default = "48h"
 }
 
 variable "sso_session_idle_timeout" {
   type = string
-  default = "48h"
 }
 
 variable "realm_role" {
   type = string
-  default = "holder"
 }
 
 variable "root_url" {
   type = string
-  default = ""   # ADD ALB ENDPOINT
 }
 
 variable "redirect_url" {
   type = string
-  default = ""    # ADD FRONTEND URL
 }
 
 variable "username" {
   type = string
-  default = "admin"
 }
 
 variable "password" {
   type = string
-  default = "admin"
 }
