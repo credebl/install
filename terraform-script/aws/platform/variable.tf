0d99d8f3f888ea5b0a84f2cc1e6894cda052818d
sahil.kamble@ayanworks.com <sahil.kamble@ayanworks.com>
2026-03-20 19:23:35 +0530

    chore: remove unwanted variables
    Signed-off-by: sahil.kamble@ayanworks.com <sahil.kamble@ayanworks.com>

--- terraform-script/aws/platform/variable.tf
+++ terraform-script/aws/platform/variable.tf
@@ -9,8 +9,6 @@ variable "certificate_arn" {}
 variable "domain_name" {}
 
 #vpc info
-variable "db_instance_class" {}
-variable "db_iops" {}
 variable "vpc_cidr" {}
 variable "public_subnet_cidr" {}
 variable "private_app_subnet_cidr" {}
